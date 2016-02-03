917249eda150f1abc24a29cc362d44b29770957d
ttimasdf <ttimasdf@gmail.com>
2016-02-03 23:15:36 +0800

    Reload theme js after page load

--- tasks/config/uglify.js
+++ tasks/config/uglify.js
@@ -8,6 +8,7 @@ module.exports = function(grunt) {
                         'jQuery',
                         'InstantClick',
                         'searchFunc',
+                        'Tranquilpeak',
                         'fancybox'
                     ]
                 }
