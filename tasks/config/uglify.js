802fa6ff582c92d5504720d1d866fa8d3bbec7aa
ttimasdf <ttimasdf@users.noreply.github.com>
2016-06-16 15:57:54 +0800

    Remove patched js function name

--- tasks/config/uglify.js
+++ tasks/config/uglify.js
@@ -11,7 +11,6 @@ module.exports = function(grunt) {
         mangle: {
           except: [
             'InstantClick',
-            'Tranquilpeak',
             'jQuery',
             'fancybox'
           ]
